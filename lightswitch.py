d64da0c8f95f02e9caa544f05062af540467d848
Tycho Hartingsveld <70889437+99062718@users.noreply.github.com>
2021-11-02 13:53:43 +0100

    made lightswitch.py functional

--- lightswitch.py
+++ lightswitch.py
@@ -1,11 +1,22 @@
 import tkinter as tk
 window = tk.Tk()
 
-button = tk.Button(text='...', bg="white", fg="black")
+button = tk.Button(text='Light is off', bg="white", fg="black")
 button.pack(pady = 20, padx = 20)
 
 # schijf hier tussen je code
+light = "off"
+window.configure(bg="black")
 
+def changeLight(event):
+    global light
+    global button
+    light = "off" if light == "on" else "on"
+    print("Light is turned off") if light == "off" else print("Light is turned on")
+    button.configure(text='Light is off') if light == "off" else button.configure(text='Light is on')
+    window.configure(bg="black") if light == "off" else window.configure(bg='yellow')
+
+button.bind('<Button>', changeLight)
 
 
 # schijf hier tussen je code
